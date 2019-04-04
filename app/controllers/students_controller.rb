b2a74491ad60a1f338c842d7487746ca181ffc26
github username <github email address>
2019-04-04 15:15:35 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,10 @@
 class StudentsController < ApplicationController
+
+  def index
+    @students = Student.search(student_params[:name])
+    #binding.pry
+  end
+
   def new
     @student = Student.new
   end
@@ -20,11 +26,8 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def index
-    @students = Student.all
-  end
-
   def student_params
+    #raise params.inspect
     params.require(:student).permit(:name, :birthday, :hometown)
   end
 end
